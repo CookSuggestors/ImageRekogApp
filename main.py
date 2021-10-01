7bb2edcd76f5e6219c7131fedbf172b65e0b89fa
hir0o <mk.9.q.kyu@gmail.com>
2021-10-01 21:14:23 +0900

    ポート番号の設定

--- main.py
+++ main.py
@@ -23,4 +23,5 @@ def index():
     return "ok"
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8887, debug=True)
+    port = int(os.getenv("PORT", 8889))
+    app.run(host="0.0.0.0", port=port)
