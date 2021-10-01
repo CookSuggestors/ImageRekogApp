06b792e8c51ca1a9781a2b421015509f8ef55d26
Yuta-Minami <s226264@wakayama-u.ac.jp>
2021-10-01 20:43:29 +0900

    flaskを用いたサーバの構築，POSTによるAPIへの画像送信，画像解析結果の出力を実装

---
+++ main.py
@@ -0,0 +1,23 @@
+from os import error
+from flask import Flask, jsonify, request
+import amazon_api as API
+
+app = Flask(__name__)
+
+@app.route('/', methods=["POST"])
+def post_img():
+    # input_url = request.args.getlist("input")
+    img = request.files['upfile'].read()
+    # img.save(img.filename)
+    # 入力
+    # 処理
+    # post_img = '/Users/Minami-Yuta/就活用/サマーインターンシップ/Optim/img/yasai.jpg'
+    Amazon_api = API.amazon_api() # インスタンス作成
+    recog_result = Amazon_api.call_api(img=img) # AmazonAPIを呼び出し，画像認識した結果のラベルリスト(json形式)
+    label_list = Amazon_api.output_label(response_label_list=recog_result) # 食材ラベルの出力
+    
+    return jsonify(label_list)
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8887, debug=True)
